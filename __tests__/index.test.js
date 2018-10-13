aa31b508dd84c8d65a0c4f688e43c5ae23248235
David Baldie <davidbaldie@Davids-MacBook-Air.local>
2018-10-13 17:40:04 -0400

    update infra

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,10 @@
 const index = require('../src/index')
+
+jest.mock('redis', () => {
+    return {
+        createClient: () => ({ get: jest.fn(), set: jest.fn() })
+    }
+})
 describe('index', () => {
     it('launches app', () => {
         expect(index.app).not.toBeNull()
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+
+const redis = require('redis')
+const redisHost = {
+    host: 'redis-server',
+    port: 6379
+}
+const client = redis.createClient(redisHost)
+client.set('visits', 0)
+
 app.get('/', (req, res) => {
-    res.status(200).send('<html><h1>Running in docker!</h1></html>')
+    client.get('visits', (err, visits) => {
+        client.set('visits', parseInt(visits) + 1)
+        res.status(200).send(`<html><h1>Visits ${visits}</h1></html>`)
+    })
 })
+
 const server = app.listen(8080, () => {
     console.log('Listening on port 8080...')
 })
+
 module.exports = { app, server }
